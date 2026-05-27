29b5546113d9e4f9a686bb697d91b1f518d9290a
IIIyJIeP <brazhnikovna@gmail.com>
2026-05-27 11:39:29 +0300

    add astroport skipSwap

--- src/blockchains/neutron/processTXs.ts
+++ src/blockchains/neutron/processTXs.ts
@@ -163,29 +163,111 @@ export async function processTxsNeutron(decodedTxs: DecodedTX[], queryClient: St
                                 'Address ', code(sender), senderDaoDaoNick, ' sold ', bold(amount.toString() + ' PHMN'), '\n'
                             )
                             countMsgs++
-                        
+
                         } else if (executeContractMsg.swap.offer_asset?.info?.native_token?.denom === denomUSDCneutron) { // #Buy
 
                             // #Buy
                             const event = indexedTx.events.find(
                                 event => event.type === 'wasm' &&
-                                event.attributes.find(attr => attr.key === 'ask_asset')?.value === denomPHMNneutron
+                                    event.attributes.find(attr => attr.key === 'ask_asset')?.value === denomPHMNneutron
                             )
                             if (!event) continue;
-                            
-                            const amount = Number(event.attributes.find(attr => attr.key === 'return_amount')?.value)/1e6
+
+                            const amount = Number(event.attributes.find(attr => attr.key === 'return_amount')?.value) / 1e6
                             if (amount < minAmountPHMN) continue;
-                            
+
                             const sender = decodedMsg.sender
                             const senderDaoDaoNick = await getDaoDaoNickname(sender)
 
-                            telegramMsg = fmt(telegramMsg, '🪙  #Neutron #Swap #Buy  💸📥🪙\n', 
+                            telegramMsg = fmt(telegramMsg, '🪙  #Neutron #Swap #Buy  💸📥🪙\n',
                                 'Address ', code(sender), senderDaoDaoNick, ' bought ', bold(amount.toString() + ' PHMN'), '\n'
                             )
                             countMsgs++
                         }
                     } else if (false) { // TODO: add/remove liquidity
 
+                    }
+                } else { // Other contracts
+                    const contractMsg = JSON.parse(new TextDecoder().decode(decodedMsg.msg)) as {
+                        swap_and_action?: {
+                            affiliates?: [],
+                            min_asset?: {
+                                native?: {
+                                    amount: string,
+                                    denom: string
+                                }
+                            },
+                            post_swap_action?: {
+                                ibc_transfer?: {
+                                    ibc_info?: {
+                                        memo?: string,
+                                        receiver?: string,
+                                        recover_address?: string,
+                                        source_channel?: string
+                                    }
+                                }
+                            },
+                            user_swap?: {
+                                swap_exact_asset_in?: {
+                                    operations?: [],
+                                    swap_venue_name?: string
+                                }
+                            },
+                            timeout_timestamp?: number
+                        }
+                    }
+
+                    if (!contractMsg.swap_and_action) continue;
+                    // #Astroport
+                    const phmnInFunds = decodedMsg.funds.find(coin => coin.denom === denomPHMNneutron)
+
+                    if (phmnInFunds) { // #Sell
+
+                        // #Sell
+                        const amount = +phmnInFunds.amount / 1e6
+                        if (amount < minAmountPHMN) continue;
+                        if (indexedTx === null) indexedTx = await getIndexedTx(queryClient, tx.txId)
+                        if (indexedTx.code !== 0) continue;
+                        const sender = decodedMsg.sender
+                        const nickNameDAODAO = await getDaoDaoNickname(sender)
+
+                        telegramMsg = fmt(telegramMsg, '🪙 #Neutron #Astroport #Swap #Sell  🪙📤💸\n',
+                            'Address ', code(sender), nickNameDAODAO, ' sold ', bold(amount.toString() + ' PHMN'), '\n'
+                        )
+
+                        countMsgs++
+
+                    } else if (contractMsg.swap_and_action?.min_asset?.native?.denom === denomPHMNneutron) { // #Buy
+
+                        // #Buy
+                        if (indexedTx === null) indexedTx = await getIndexedTx(queryClient, tx.txId)
+                        if (indexedTx.code !== 0) continue;
+                        const amount = +(indexedTx.events.find(
+                            ev => ev.type === 'token_swapped' && ev.attributes.find(
+                                attr => attr.key === 'tokens_out'
+                            )?.value.includes(denomPHMNneutron)
+                        )?.attributes.find(
+                            attr => attr.key === 'tokens_out'
+                        )?.value.replace(denomPHMNneutron, '') || '0') / 1e6
+                        if (amount < minAmountPHMN) continue;
+
+                        const sender = decodedMsg.sender
+                        const nickNameDAODAO = await getDaoDaoNickname(sender)
+
+                        telegramMsg = fmt(telegramMsg, '🪙 #Neutron #Astroport #Swap #Buy  💸📥🪙\n',
+                            'Address ', code(sender), nickNameDAODAO, ' bought ', bold(amount.toString() + ' PHMN'), '\n'
+                        )
+                        countMsgs++
+
+                        // #IBCtransfer
+                        const ibcInfo = contractMsg.swap_and_action.post_swap_action?.ibc_transfer?.ibc_info
+                        if (!ibcInfo) continue;
+                        const receiver = getReceiverFromMemo(ibcInfo.memo || "") || ibcInfo.receiver
+                        if (!receiver) continue;
+                        const receiverDaoDaoNick = await getDaoDaoNickname(receiver)
+                        telegramMsg = fmt(telegramMsg, '🪙 #IBCtransfer  📬\n',
+                            '& sent over IBC protocol to ', code(receiver), receiverDaoDaoNick, '\n',
+                        )
                     }
                 }
             }
